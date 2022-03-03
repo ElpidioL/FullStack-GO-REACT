cc2f02255e9e72631a7377c6860230da2dd4b6f4
ElpidioL <lomeuelpidio@gmail.com>
2022-03-02 21:56:23 -0300

    Update LoginUser.go

--- backend/PSDB/LoginUser.go
+++ backend/PSDB/LoginUser.go
@@ -9,19 +9,15 @@ import (
 )
 
 func LoginUser(email string, password string, hashToken string) error {
-	//storing the info to access the DB
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		DbInfo.Host, DbInfo.Port, DbInfo.User, DbInfo.Password, DbInfo.Dbname)
 
-	//starting db
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
-	//don't forget to close it
 	defer db.Close()
-	//ping to check if its working
 	err = db.Ping()
 	if err != nil {
 		panic(err)
